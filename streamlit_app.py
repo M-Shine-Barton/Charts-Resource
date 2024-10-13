dd3d1ee181e65203d0b013b2387721c069ef8d8b
M-Shine-Barton <shortknockerxsm@gmail.com>
2024-10-13 15:59:16 -0400

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Your MULTIPLE Weekly Chart Resource", layout='wide')
+
+# --- header section ---
+st.subheader("SECTOR: Consumer Discretionary")
+st.title("INDUSTRIES: ")
+st.write("[View Charts >](https://propweeklychart.com)"
